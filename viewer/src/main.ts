bd404c5041f18e300fe43100d8accb1e62ad14fc
0b5vr <0b5vr@0b5vr.com>
2026-04-16 16:00:07 +0900

    feat, deps: viewer, import three, populate with gltf scene

--- viewer/src/main.ts
+++ viewer/src/main.ts
@@ -1,24 +1,31 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import './style.css';
+import sampleGltf from './assets/twist-sample-with-khr.glb?url';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 20);
+camera.position.set(0, 1, 3);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 1, 0);
+controls.update();
+
+const scene = new THREE.Scene();
+
+const light = new THREE.DirectionalLight(0xffffff, Math.PI);
+light.position.set(1, 2, 3);
+scene.add(light);
+
+const loader = new GLTFLoader();
+const gltf = await loader.loadAsync(sampleGltf);
+scene.add(gltf.scene);
+
+renderer.setAnimationLoop(() => {
+  renderer.render(scene, camera);
+});
