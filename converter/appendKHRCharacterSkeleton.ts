3dec0536d37a71ea1f461f34d760af303664e54e
0b5vr <0b5vr@0b5vr.com>
2026-06-01 14:19:03 +0900

    fix: converter, Skip eye bones when generating KHR_character_skeleton_mapping
    In viewer example, the eye bones animation from the humanoid side conflicts with the look expression, causing the eyes to not look in the correct direction. This commit will fix the issue by skipping the eye bones when generating the vrm skeletal rig mapping for KHR_character_skeleton_mapping.
    
    `appendBoneLookExpression` references the source vrmHumanoid instead of the converted skeleton mapping, look expressions should be generated even after the change.
    
    Also updated the sample model in viewer using the fixed converter.

--- converter/appendKHRCharacterSkeleton.ts
+++ converter/appendKHRCharacterSkeleton.ts
@@ -1,7 +1,17 @@
 import { logVerbose } from './logVerbose.ts';
 import type { GLTF } from '@gltf-transform/core';
 import type { KHRCharacterSkeletonMapping, KHRCharacterSkeletonMappingSkeletalRigMapping } from '../schematypes/KHRCharacterSkeletonMapping.ts';
-import type { VRMCVRM } from '@pixiv/types-vrmc-vrm-1.0';
+import type { HumanoidHumanBoneName, VRMCVRM } from '@pixiv/types-vrmc-vrm-1.0';
+
+/**
+ * In KHR_character, eye rotations are took place in the expression system, so setting eye bones in
+ * the skeletal rig mapping may conflict with the expression system and cause issues.
+ * To avoid this, we skip setting eye bones in the `vrmHumanoid` mapping.
+ */
+const vrmBonesToSkip: Set<HumanoidHumanBoneName> = new Set([
+  'leftEye',
+  'rightEye',
+]);
 
 export function appendKHRCharacterSkeleton(gltf: GLTF.IGLTF): void {
   const vrm = gltf.extensions?.['VRMC_vrm'] as VRMCVRM | undefined;
@@ -17,6 +27,11 @@ export function appendKHRCharacterSkeleton(gltf: GLTF.IGLTF): void {
   logVerbose(`KHR_character_skeleton_mapping: Mapping VRM humanoid bones to ${mappingName} mapping`);
 
   for (const [boneName, humanBone] of Object.entries(vrmHumanoid.humanBones)) {
+    if (vrmBonesToSkip.has(boneName as HumanoidHumanBoneName)) {
+      logVerbose(`KHR_character_skeleton_mapping: "${mappingName}" mapping, skipping bone "${boneName}"`);
+      continue;
+    }
+
     const node = nodes?.[humanBone.node];
     const nodeName = node?.name;
     if (node != null && nodeName) {
