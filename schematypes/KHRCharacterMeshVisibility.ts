ee5309345cd99eeb33641fa9f56f81bebe38254d
0b5vr <0b5vr@0b5vr.com>
2026-06-30 14:27:27 +0900

    feat: try KHR_character_mesh_visibility and KHR_character_node_visibility
    to hide head meshes on first person view
    
    The extension name and property names are tentative
    
    the commit point that KHR_mesh_annotation_renderview existed: https://github.com/KhronosGroup/glTF/pull/2512/changes/8caa03010749cbfc8e179d50918535345493c83f

---
+++ schematypes/KHRCharacterMeshVisibility.ts
@@ -0,0 +1,3 @@
+export interface KHRCharacterMeshVisibility {
+  visibility: 'firstPerson' | 'thirdPerson' | 'always';
+}
---
+++ schematypes/KHRCharacterNodeVisibility.ts
@@ -0,0 +1,3 @@
+export interface KHRCharacterNodeVisibility {
+  visibility: 'firstPerson' | 'thirdPerson' | 'always';
+}
